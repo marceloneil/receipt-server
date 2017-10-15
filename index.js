e46d7cdca9386ca8d29f332b03de4ae279760841
Marcel O'Neil <marcel@marceloneil.com>
2017-10-14 23:47:35 -0400

    fix

--- index.js
+++ index.js
@@ -10,13 +10,15 @@ app.use(Express.static(path.join(__dirname, 'dapp/build')))
 const web3 = new Web3()
 web3.setProvider(new web3.providers.HttpProvider('http://geth:8545'))
 
-for (let i = 1950; i < 1960; i++) {
-  web3.eth.getBlock(i).then(data => {
-    console.log(data)
-  }).catch(error => {
-    console.log(error)
-  })
-}
+setTimeout(() => {
+  for (let i = 1950; i < 1960; i++) {
+    web3.eth.getBlock(i).then(data => {
+      console.log(data)
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+}, 10000)
 
 app.post('/receipt', function (req, res) {
   console.log(req.body)
